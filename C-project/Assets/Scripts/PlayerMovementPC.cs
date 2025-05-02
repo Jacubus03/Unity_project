bddb400e8ea1b3f2cdc0e231cc1b8b982192ffef
Jacubus03 <jakubgorski1212@gmail.com>
2025-05-02 23:36:50 +0200

    VR Project Setup

--- C-project/Assets/Scripts/PlayerMovementPC.cs
+++ C-project/Assets/Scripts/PlayerMovementPC.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class PlayerMovementPC : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(Time.deltaTime, 0, 0));
+        // Get input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Move in the direction relative to player
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+        // Apply movement
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Apply gravity
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
     }
 }
